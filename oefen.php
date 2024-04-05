ab8c79e49f3ceb1e73bd5a62fea6bb576b5a0969
Anouk2005 <515046@vistacollege.nl>
2024-04-05 11:27:12 +0200

    leren

---
+++ oefen.php
@@ -0,0 +1,18 @@
+<?php
+Function greetings
+$name= "John";
+echo= "hello $name";
+
+Function rijbewijs
+$age= 16
+If ($age=> 16) {
+    echo= ( "you are old enough";)}
+Else = {("you're not old enough")}
+
+
+
+
+
+
+
+?>
