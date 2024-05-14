a6b5e353ea292f55b6185182d55bb978cfa37861
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2024-05-14 16:59:04 +0200

    added database logistiscs
    this doesnt include the database itself or its connection credentials. this is just a pdo connection to any database

--- controllers/database.php
+++ controllers/database.php
@@ -2,12 +2,33 @@
 
 namespace Eigenheimer\controllers;
 
+use PDO;
+use PDOException;
+
 class connect_Database
 {
   private function connect_main()
   {
-    $bro = 'connected to database!';
-    return $bro;
+
+  // Database credentials
+  $servername = "localhost"; // Replace with your MySQL server address
+  $username = "username"; // Replace with your MySQL username
+  $password = "password"; // Replace with your MySQL password
+  $dbname = "database"; // Replace with your MySQL database name
+
+    try {
+      // Create connection
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      
+      // Set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+      return $conn; // Return the connection object
+      
+      } 
+    catch(PDOException $e) {
+      return $e; // Return the exception object
+    }
   }
   
   public static function query_order_food()
@@ -17,3 +38,4 @@ class connect_Database
   }
   
 }
+
