8722a2fc8359d51df13e951ef54fe1809b9bc796
HetGameBoekje <48868491+hetgameboekje@users.noreply.github.com>
2024-05-14 15:06:15 +0200

    able to add and clear the array(basket)

--- model/home.php
+++ model/home.php
@@ -6,19 +6,23 @@
   session_start(); // Start or resume a session
 
   if ($_GET['action'] === 'additem' && isset($_GET['id'])) {
-      
+      if($_GET['id'] < 0){
+        session_destroy();
+        header('location:'.$url = preg_replace('/additem&id=-1/', 'view', $_SERVER['REQUEST_URI']));
+      }
       if (!isset($_SESSION['ids'])) {
           $_SESSION['ids'] = []; // Initialize the array if it's not initialized already
       }
       $_SESSION['ids'][] = $_GET['id']; // Add the new ID to the array
       var_dump($_SESSION['ids']);
- 
-      
   }
+  
+
   UI::navbar();
   UI::items(100);
 
 
-  
+
+
 ?>
 
